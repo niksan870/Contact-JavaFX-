d7b84c3cec8eea1c68dddf8d57c0f01664ddb6ad
niksan870 <niksan870@abv.bg>
2019-09-26 15:29:14 +0300

    first commit

---
+++ src/sample/ContactController.java
@@ -0,0 +1,54 @@
+package sample;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import sample.datamodel.Contact;
+
+public class ContactController{
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField notesField;
+
+    public boolean validateNewContact(){
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        String notes = notesField.getText();
+
+        return (firstName.isEmpty() || lastName.isEmpty() || phoneNumber.isEmpty() || notes.isEmpty()) ? false : true;
+    }
+
+    public Contact getNewContact() {
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        String notes = notesField.getText();
+
+        Contact newContact = new Contact(firstName, lastName, phoneNumber, notes);
+        return newContact;
+    }
+
+    public void editContact(Contact contact){
+        firstNameField.setText(contact.getFirstName());
+        lastNameField.setText(contact.getLastName());
+        phoneNumberField.setText(contact.getPhoneNumber());
+        notesField.setText(contact.getNotes());
+    }
+
+    public void updateContact(Contact contact){
+        contact.setFirstName(firstNameField.getText());
+        contact.setLastName(lastNameField.getText());
+        contact.setPhoneNumber(phoneNumberField.getText());
+        contact.setNotes(notesField.getText());
+    }
+}
